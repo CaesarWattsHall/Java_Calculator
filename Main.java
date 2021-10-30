271d052a25d5ab3841479675996561a401390180
Caesar R. Watts-Hall <93171607+CaesarWattsHall@users.noreply.github.com>
2021-10-29 20:43:47 -0500

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,14 @@
+//START
+package lab3_jtabbedpane;
+import javax.swing.*;
+import java.awt.*;
+
+public class Lab3_JTabbedPane 
+{
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        CalculatorTabbedPane ctp = new CalculatorTabbedPane();
+    } 
+}
+//END
